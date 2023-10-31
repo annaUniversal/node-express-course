76af1e84137df084241d5f62855b37441ff962af
annaUniversal <anna.universal@yahoo.com>
2023-10-31 11:18:48 -0400

    week5 DB connection for Task Manager

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,32 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routs/tasks");
+const connectDB = require("./db/connect");
+require('dotenv').config()
+
+//middleware
+app.use(express.json()); //to parse the incoming request body in middleware
+
+//routs
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks); //get all tasks
+app.post("/api/v1/tasks", tasks); //create a new tast
+
+//update tasks
+app.get("/api/v1/tasks/:id", (req, res) => {});
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MANGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ 03-task-manager/starter/routs/tasks.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTasks,
+  deleteTasks,
+} = require("../controllers/tasks");
+
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTasks).delete(deleteTasks);
+
+
+module.exports = router;
