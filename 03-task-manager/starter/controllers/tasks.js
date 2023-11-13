bdba9b6fcddc4fd4fa544a6ab13e21007673b0fc
annaUniversal <anna.universal@yahoo.com>
2023-11-12 19:15:12 -0500

    added new for creating object CustomAPIError

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -30,7 +30,7 @@ const deleteTasks = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return next(createCustomError(`No task with id ${taskID} found`,404));
+    return next(new createCustomError(`No task with id ${taskID} found`,404));
   }
   res.status(200).json({ task });
   // res.status(200).send());
